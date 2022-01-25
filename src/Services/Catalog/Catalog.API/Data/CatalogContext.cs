28341b0d72e9526d1584566633833c69cbb279b9
amolhekade <aphekade@gmail.com>
2022-01-25 18:36:00 +0530

    XXXXXXX

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var clinet = new MongoClient(configuration.GetValue <string>("DatabseSettings:ConnectionStrings"));
+            var database = clinet.GetDatabase(configuration.GetValue<string>("DatabseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabseSettings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+    }
+}
